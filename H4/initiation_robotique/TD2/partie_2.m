cb658ceeedfdfb1b05b88ca656ac0720731c3034
Simon <71031475+apoleon33@users.noreply.github.com>
2025-10-30 22:34:48 +0100

    finished part 2

--- H4/initiation_robotique/TD2/partie_2.m
+++ H4/initiation_robotique/TD2/partie_2.m
@@ -32,6 +32,77 @@ legend('Signal biaisé', 'Signal compensé');
 
 % Question 8
 % cap biaise
+Tech = 0.02;
+psi0 = -2.18;
+BiasedCap = zeros(length(Tcapt), 1);
+UnbiasedCap = zeros(length(Tcapt), 1);
+
+BiasedCap(1) = psi0;
+UnbiasedCap(1) = psi0;
+
+for n = 1:length(Tcapt)-1
+    BiasedCap(n+1)      = BiasedCap(n)      + Tech * Psip(n);
+    UnbiasedCap(n+1) = UnbiasedCap(n) + Tech * UnbiasedPsip(n);
+end
+
 fig8 = figure;
-biasedCap = integral(@(p) Psip(p), Tcapt(1), Tcapt(end));
-plot(biasedCap);
+plot(Tcapt, BiasedCap);
+hold on;
+plot(Tcapt, UnbiasedCap); % accumulation d'erreur
+
+title('Évolution du cap');
+legend('Cap biaisé', 'cap non biaisé');
+
+% Question 9
+x0 = 0;
+y0 = 0;
+
+bX = zeros(length(Tcapt), 1); % x biaise
+bY = zeros(length(Tcapt), 1); % y biaiser
+
+nX = zeros(length(Tcapt), 1); % x non biaise
+nY = zeros(length(Tcapt), 1); % y non biaiser
+
+for n = 1:length(Tcapt)-1
+  bX(n+1) = bX(n) + Tech*(Vl(n)*cos(BiasedCap(n)) - Vt(n)*sin(BiasedCap(n)));
+  bY(n+1) = bY(n) + Tech*(Vl(n)*sin(BiasedCap(n)) + Vt(n)*cos(BiasedCap(n)));
+
+  nX(n+1) = nX(n) + Tech*(Vl(n)*cos(UnbiasedCap(n)) - Vt(n)*sin(UnbiasedCap(n)));
+  nY(n+1) = nY(n) + Tech*(Vl(n)*sin(UnbiasedCap(n)) + Vt(n)*cos(UnbiasedCap(n)));
+end
+
+fig8 = figure;
+plot(bX, bY, 'b');
+hold on;
+plot(nX, nY, 'r');
+
+title('Évolution des trajectoires biaisée et non-biaisée');
+legend('trajectoire biaisée', 'trajectoire non-biaisée');
+
+% Question 10
+
+fig10 = figure;
+grid on;
+hold on                 %  Maintenir active la figure cr??e pour ajout de trac?s
+plot(x1,y1,'m.')        % Tracé du bord droit du circuit non interpol? en jaune
+plot(x2,y2,'c.')        % Tracé du bord gauche du circuit non interpol? en cyan
+plot(Xgps, Ygps, 'r.');
+plot(nX, nY, 'b.');
+axis equal;             % Repère orthonormé
+xlabel('x (m)')         % Abscisses en metre
+ylabel('y (m)')         % Ordonnées en metre
+
+title('Évolution des trajectoires sur le circuit') % Titre
+legend('bord droit','bord gauche', 'Trajectoire GPS', 'Trajectoire DR corrigée') % Légende
+
+% Question 11
+ecartX = Xgps - nX;
+ecartY = Ygps - nY;
+
+fig11 = figure;
+plot(ecartX);
+hold on;
+plot(ecartY);
+ecartMoyen = [mean(ecartX) mean(ecartY)]
+title('Évolution des ecarts GPS-DR');
+legend('écart en X', 'Écart en Y');
